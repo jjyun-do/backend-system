206affd01744806bf2e2f5ea3c0c4391dc9e6d75
도지윤/Data Research팀(SR)/삼성전자 <jjyun.do@samsung.com>
2022-03-03 18:00:03 +0900

    :white_check_mark: To apply consistent coding style, add ktlint and detekt (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.4" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.20.0-RC1" apply false
+
     kotlin("jvm") version "1.6.10" apply false
     kotlin("plugin.spring") version "1.6.10" apply false
 }
@@ -32,8 +35,10 @@ allprojects {
     }
 }
 
-subprojects{
+subprojects {
     apply {
         plugin("io.spring.dependency-management")
+        plugin("org.jlleitschuh.gradle.ktlint")
+        plugin("io.gitlab.arturbosch.detekt")
     }
 }
