f99a66bbce17b776b671e6a54a6d1fff5db6afa1
도지윤/Data Research팀(SR)/삼성전자 <jjyun.do@samsung.com>
2022-06-27 12:45:03 +0900

    :recycle: Change the type of exception (#71)

--- platform/src/main/kotlin/com/samsung/healthcare/platform/application/service/UserProfileService.kt
+++ platform/src/main/kotlin/com/samsung/healthcare/platform/application/service/UserProfileService.kt
@@ -1,5 +1,7 @@
 package com.samsung.healthcare.platform.application.service
 
+import com.samsung.healthcare.platform.adapter.web.context.ContextHolder.getFirebaseToken
+import com.samsung.healthcare.platform.application.exception.ForbiddenException
 import com.samsung.healthcare.platform.application.port.input.UserProfileInputPort
 import com.samsung.healthcare.platform.application.port.output.UserProfileOutputPort
 import com.samsung.healthcare.platform.domain.project.UserProfile
@@ -10,6 +12,9 @@ class UserProfileService(
     private val userProfileOutputPort: UserProfileOutputPort
 ) : UserProfileInputPort {
     override suspend fun registerUser(userProfile: UserProfile) {
+        if (userProfile.userId != getFirebaseToken().uid)
+            throw ForbiddenException()
+
         userProfileOutputPort.create(userProfile)
     }
 }
