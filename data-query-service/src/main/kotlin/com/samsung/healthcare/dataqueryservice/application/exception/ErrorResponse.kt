0c192e37b0ee0283d38b5fc009a3510ab1ef798e
곽창근/Data Research팀(SR)/삼성전자 <culater.kwak@samsung.com>
2022-07-29 16:37:40 +0900

    :sparkles: Add debug error handling option. (#75)
    * :sparkles: Add debug error handling option.
    
    On DEBUG mode, GlobalExceptionHandler returns additional JSON field, 'reason'. 'reason' field will include exception message.
    Plus, 'code' field will store our custom error codes and 'message' field will store brief error message to show to users.
    'timestamp' field will store time that an error has occurred.
    
    Signed-off-by: culater.kwak <culater.kwak@samsung.com>
    
    * :recycle: applied jidon's suggestion.
    
    Before, it judged 'DEBUG' mode by looking up properties file's 'config.debug' value.
    Fow now, it looks 'debug' environment value or properties file's 'debug' value.
    
    Signed-off-by: culater.kwak <culater.kwak@samsung.com>
    
    * :recycle: Removed unused import
    
    Signed-off-by: culater.kwak <culater.kwak@samsung.com>

--- data-query-service/src/main/kotlin/com/samsung/healthcare/dataqueryservice/application/exception/ErrorResponse.kt
+++ data-query-service/src/main/kotlin/com/samsung/healthcare/dataqueryservice/application/exception/ErrorResponse.kt
@@ -1,6 +1,13 @@
 package com.samsung.healthcare.dataqueryservice.application.exception
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import java.time.LocalDateTime
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class ErrorResponse(
-    val code: String,
-    val message: String?,
+    val timestamp: LocalDateTime = LocalDateTime.now(),
+    // TODO: Decide code types
+    val code: String = "TODO",
+    val message: String,
+    val reason: String?,
 )
