f342fb5a6ce75b631cf5447660a8a67b324ae26c
김종문/Data Research팀(SR)/삼성전자 <jlego229.kim@samsung.com>
2022-09-30 10:55:57 +0900

    add optional fields and basic validation logics (#152)
    Co-authored-by: jlego229-kim <jlego229.kim@samsung.net>

--- platform/src/main/kotlin/com/samsung/healthcare/platform/application/port/input/project/task/UpdateTaskCommand.kt
+++ platform/src/main/kotlin/com/samsung/healthcare/platform/application/port/input/project/task/UpdateTaskCommand.kt
@@ -6,28 +6,42 @@ import java.time.LocalDateTime
 
 data class UpdateTaskCommand(
     val title: String,
-    val description: String,
-    val schedule: String,
-    val startTime: LocalDateTime,
-    val endTime: LocalDateTime,
-    val validTime: Int,
+    val description: String? = null,
+    val schedule: String? = null,
+    val startTime: LocalDateTime? = null,
+    var endTime: LocalDateTime? = null,
+    val validTime: Int? = null,
     val status: TaskStatus,
     val items: List<UpdateItemCommand>,
     val condition: Map<String, Any> = emptyMap(),
 ) {
+    companion object {
+        private const val DEFAULT_END_TIME_MONTH: Long = 3
+    }
     // TODO: validate
+    init {
+        if (status == TaskStatus.PUBLISHED) {
+            requireNotNull(schedule)
+            requireNotNull(startTime)
+            requireNotNull(validTime)
+            if (endTime == null)
+                endTime = startTime.plusMonths(DEFAULT_END_TIME_MONTH)
+        }
+    }
+
     data class UpdateItemCommand(
         val contents: Map<String, Any>,
         val type: ItemType,
         val sequence: Int
     )
 
-    val properties = mapOf<String, Any>(
+    @Suppress("UNCHECKED_CAST")
+    val properties = mapOf<String, Any?>(
         "title" to title,
         "description" to description,
         "schedule" to schedule,
         "startTime" to startTime,
         "endTime" to endTime,
         "validTime" to validTime
-    )
+    ).filterValues { it != null } as Map<String, Any>
 }
