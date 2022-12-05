7d72213ad31f09451691660a4d046b2ba8e53a17
최성재/Data Research팀(SR)/삼성전자 <cubist.choi@samsung.com>
2022-12-05 13:51:32 +0900

    :white_check_mark: Add negative test to SchedulingServiceTest

--- trino-rule-update-service/src/test/kotlin/com/samsung/healthcare/trinoruleupdateservice/application/service/SchedulingServiceTest.kt
+++ trino-rule-update-service/src/test/kotlin/com/samsung/healthcare/trinoruleupdateservice/application/service/SchedulingServiceTest.kt
@@ -1,10 +1,12 @@
 package com.samsung.healthcare.trinoruleupdateservice.application.service
 
+import com.google.gson.JsonIOException
+import com.samsung.healthcare.trinoruleupdateservice.NEGATIVE_TEST
 import com.samsung.healthcare.trinoruleupdateservice.POSITIVE_TEST
 import com.samsung.healthcare.trinoruleupdateservice.application.config.ApplicationProperties
-import com.samsung.healthcare.trinoruleupdateservice.application.config.ApplicationProperties.DatabaseConfig
 import com.samsung.healthcare.trinoruleupdateservice.application.config.ApplicationProperties.AccessControlConfig
 import com.samsung.healthcare.trinoruleupdateservice.application.config.ApplicationProperties.AccountServiceConfig
+import com.samsung.healthcare.trinoruleupdateservice.application.config.ApplicationProperties.DatabaseConfig
 import com.samsung.healthcare.trinoruleupdateservice.application.config.ApplicationProperties.TrinoConfig
 import com.samsung.healthcare.trinoruleupdateservice.application.port.output.GetUsersPort
 import com.samsung.healthcare.trinoruleupdateservice.application.port.output.UpdateRulePort
@@ -12,6 +14,7 @@ import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class SchedulingServiceTest {
     private val getUsersPort = mockk<GetUsersPort>()
@@ -33,5 +36,15 @@ internal class SchedulingServiceTest {
         every { updateRulePort.updateAccessControlConfigFile(any()) } returns Unit
 
         schedulingService.fetchUsers()
+    }
+
+    @Test
+    @Tag(NEGATIVE_TEST)
+    fun `fetchUsers should raise JsonIOException when the rule is broken`() {
+
+        every { getUsersPort.getUsers() } returns emptyList()
+        every { updateRulePort.updateAccessControlConfigFile(any()) } throws JsonIOException("error msg")
+
+        assertThrows<JsonIOException> { schedulingService.fetchUsers() }
     }
 }
