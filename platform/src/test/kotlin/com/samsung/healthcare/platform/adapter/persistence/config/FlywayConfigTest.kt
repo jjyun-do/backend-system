64f5092e9a3ddbfb02ba84a70bbec87350e94581
최성재/Data Research팀(SR)/삼성전자 <cubist.choi@samsung.com>
2022-12-09 14:05:04 +0900

    :white_check_mark: Add ProjectDatabaseAdapterTest and ProjectTest (#193)
    * :white_check_mark: Add ProjectDatabaseAdapterTest
    
    * :white_check_mark: Update visibility of FlywayConfigTest
    
    * :white_check_mark: Add ProjectTest

--- platform/src/test/kotlin/com/samsung/healthcare/platform/adapter/persistence/config/FlywayConfigTest.kt
+++ platform/src/test/kotlin/com/samsung/healthcare/platform/adapter/persistence/config/FlywayConfigTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class FlywayConfigTest {
+internal class FlywayConfigTest {
 
     private val flywayClassicConfig = mockk<ClassicConfiguration>()
 
